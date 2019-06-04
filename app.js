b63416f500ce8c27ac2523e15af051f826f1d144
rooz509 <40474586+rooz509@users.noreply.github.com>
2019-06-04 12:23:55 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,103 +1,65 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
-const ColorService = require('./services/color-service.js');
+const ColorService = require('./services/color-service');
 
-//routing
 $(document).ready(() => {
-   $('#btnSearch').on('click', () => {
-        let txtSearch = $('#txtSearch').val();
+  $('#searchBtn').on('click', () => {
+    const searchText = $('#searchText').val();
 
-        ColorService.searchColors(txtSearch)
-            .then((results) => {
-                const paletteTemplate = $('#paletteTemplate');
-                const paletteHtml = paletteTemplate.html().trim();
-                const output = $('#output');
+    ColorService
+      .searchColors(searchText)
+      .then((results) => {
+        const paletteTemplate = $('#paletteTemplate');
+        const paletteHTML = paletteTemplate.html().trim();
+        const output = $('#output');
 
-                results.forEach((palette) => {
+        results.forEach((palette) => {
+          const $palette = $(paletteHTML);
 
-                    let $palette = $(paletteHtml);
+          const $paletteImage = $palette.find('.palette-image');
+          const $paletteName = $palette.find('.palette-name');
+          const $paletteAuthor = $palette.find('.palette-author');
 
-                    //update palette img
-                    let $image = $palette.find('.p-img');
-                    $image.attr('src', palette.imageUrl);
+          $paletteImage.attr('src', palette.imageUrl);
+          $paletteName.text(palette.title);
+          $paletteAuthor.text(palette.userName);
 
-                    //update palette name
-                    let $name = $palette.find('.p-name');
-                    $name.text(palette.title);
+          output.append($palette);
+        });
+      })
+      .catch((err) => {
+        /* eslint-disable */
+        alert('shit hit the fan');
+        console.error('failed: ', err);
+        /* eslint-enable */
+      });
+  });
+});
 
-                    //update palette author
-                    let $author = $palette.find('.p-author');
-                    $author.text(palette.userName);
+},{"./services/color-service":2}],2:[function(require,module,exports){
+const colourLoversAPI = '//www.colourlovers.com/api/colors';
 
-                    output.append($palette);
-                });
-            })
-            .catch((err) => {
-                alert('you effed it up, its all your fault');
-                console.error('failed: ', err);
-            })
-   });
-} );
-        //let searchURL = `${colourLoversAPI}?keywords=${txtSearch}&jsonCallback=?`;
+function searchColors(query, filters = {}) {
+  const queryParams = [];
 
-        // $.getJSON(searchURL, (results) => {
-        //     console.log('results: ', results);
-        //     const paletteTemplate = $('#paletteTemplate');
-        //     const paletteHtml = paletteTemplate.html().trim();
-        //     const output = $('#output');
-        //
-        //     results.forEach((palette) => {
-        //
-        //         let $palette = $(paletteHtml);
-        //
-        //         //update palette img
-        //         let $image = $palette.find('.p-img');
-        //         $image.attr('src', palette.imageUrl);
-        //
-        //         //update palette name
-        //         let $name = $palette.find('.p-name');
-        //         $name.text(palette.title);
-        //
-        //         //update palette author
-        //         let $author = $palette.find('.p-author');
-        //         $author.text(palette.userName);
-        //
-        //         output.append($palette);
-        //     });
-        // });
+  /* eslint-disable-next-line */
+  for (const key in Object.keys(filters)) {
+    queryParams.push(`${key}=${filters[key]}`);
+  }
+  queryParams.push(`keywords=${query}`);
+  queryParams.push('jsonCallback=?');
 
-},{"./services/color-service.js":2}],2:[function(require,module,exports){
+  const searchUrl = `${colourLoversAPI}?${queryParams.join('&')}`;
+
+  return new Promise((resolve, reject) => {
+    $.getJSON(searchUrl, resolve)
+      .fail((jqxhr, textStatus, error) => {
+        reject(error);
+      });
+  });
+}
 
 module.exports = {
-    getColors: getColors,
-    searchColors: searchColors
+  searchColors,
 };
 
-const colourLoversAPI = 'http://www.colourlovers.com/api/colors';
-
-function getColors()
-{
-
-}
-
-function searchColors(query, filters = {})
-{
-    let queryParams = [];
-    for(let key in filters)
-    {
-        queryParams.push(`${key}=${filters[key]}`);
-    }
-    queryParams.push(`keywords=${query}`);
-    queryParams.push('jsonCallback=?');
-
-    let searchURL = `${colourLoversAPI}?${queryParams.join('&')}`;
-
-    return new Promise((resolve, reject) => {
-        $.getJSON(searchURL, resolve)
-            .fail((jqxhr, textStatus, error) => {
-                reject(error);
-            });
-    });
-
-
-}
 },{}]},{},[1]);
